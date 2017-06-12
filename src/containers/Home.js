d99241a570c6138053388981f9f4dfa2e175fe63
calledandrey <andrey0405@list.ru>
2017-06-12 18:44:52 +0300

    change homepage text for testing Heroku build script

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
     <Container>
         <Row>
             <h1 className="container-item">Welcome to the Club</h1>
-            <p className="container-item">Lets Rock!</p>
+            <p className="container-item">Lets Rock!!!</p>
         </Row>
     </Container>
 )
