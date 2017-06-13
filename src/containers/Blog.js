7865395c4a78c90414836a69363bcb6463511a68
Dmitry Lebedev <p17dmitry@gmail.com>
2017-06-13 14:33:50 +0300

    Display blog posts from fake api

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { fetchBlogPostsIfNeeded } from '../store/actions/blogPostsActions'
 
-const Blog = () => (
-    <div>freeCodeCamp e-commerce project{'\''}s blog</div>
+class Blog extends Component {
+    componentDidMount() {
+        this.props.fetchBlogPosts()
+    }
+    render() {
+        const { blogPosts, fetchError } = this.props
+        return (
+            <div>
+                <h1>freeCodeCamp e-commerce project{'\''}s blog</h1>
+                { fetchError && <p>Could not get new blog posts.</p> }
+                <ul>
+                    { blogPosts.map(blogPost => (
+                        <li key={`blog-post-${blogPost.id}`}>
+                            <p>Id: {blogPost.id}</p>
+                            <p>Title: {blogPost.title}</p>
+                            <p>Archived: {blogPost.archived.toString()}</p>
+                            <p>Article: {blogPost.article}</p>
+                            Cover: <img src={blogPost.cover} alt="Cover" />
+                            <p>Created at: {new Date(blogPost.publishedAt * 1000).toString()}</p>
+                            <p>Permalink: {blogPost.permalink}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+Blog.propTypes = {
+    blogPosts: PropTypes.array.isRequired,
+    fetchError: PropTypes.string.isRequired,
+    fetchBlogPosts: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => (
+    {
+        blogPosts: state.blogPosts.items,
+        fetchError: state.blogPosts.error
+    }
 )
 
-export default Blog
+const mapDispatchToProps = (dispatch) => (
+    {
+        fetchBlogPosts: () => {
+            dispatch(fetchBlogPostsIfNeeded())
+        }
+    }
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Blog)
